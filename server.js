fccbfcbe6452d3d66407e76d32a67f427ba776ff
krishu9868 <120005592+krishu9868@users.noreply.github.com>
2023-08-28 21:20:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ let {productRoutes} = require('./Routers/product.router')
 let {connection} = require("./db")
 let cors = require('cors')
 require('dotenv').config()
-app.use(cors())
+
 app.use(express.json())
+
+const allowedOrigins = ['https://deluxe-daifuku-1dda8e.netlify.app/'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 
 app.get("/",(req,res)=>{
     res.send({msg:"Welcome"})
